4a8dc3083081b67ea762831c6a13ac25e621ea73
sahilangral97 <sahil16082@iiitd.ac.in>
2020-12-11 18:46:21 +0530

    as

--- index.js
+++ index.js
@@ -11,16 +11,15 @@ await page.goto(url)
 // console.log({srctxt})
 //await page.waitFor(4000);
 //await page.click('a[title="Ingles"]')
-await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.3.1.slim.min.js'});
-await page.setDefaultNavigationTimeout(0)
-
-
+await page.evaluate(() => {
+    doGTranslate('pt|en')
+  });
+await page.waitFor(6000)
 
 const text1 = await page.evaluate(() => document.querySelectorAll("span.lbl-licitacao")[0].textContent.trim())
 const text2 = await page.evaluate(() => document.querySelectorAll("p")[0].textContent.trim())
 const text6 = await page.evaluate(() => document.querySelectorAll("span.lbl-licitacao")[5].textContent.trim())
 
-//*[@id="block-views-block-view-noticia-pbh-block-5"]/div/div/div/div/div/div[3]/div/table/tbody/tr/td[2]/div/div/div/a
 
 console.log( text1);
 console.log(text2);
@@ -28,10 +27,16 @@ console.log(text6);
 
 const [elements ]= await page.$x('//*[@id="block-views-block-view-noticia-pbh-block-5"]/div/div/div/div/div/div[3]/div/table/tbody/tr/td[2]/div/div/div/a')
 const link = await elements.getProperty('href')
-console.log(link._remoteObject.value)
+const imp_link = link._remoteObject.value
+console.log(`Related Links: ${imp_link}`)
 
 }
 
+
+
+
+
+ 
 
 
 scrapProduct("https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020")
