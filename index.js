8f4a5ee911c0f04995a9e9ea4655fbe5349c5fcb
sahilangral97 <sahil16082@iiitd.ac.in>
2020-12-11 19:14:27 +0530

    Final

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const browser = await puppteer.launch()
 const page = await browser.newPage()
 await page.goto(url)
 
-// const [el] = await page.$x('/html/body/div[2]/div/div[5]/div/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div/div/div[2]/span/span[1]/font/font')
-// const src = await el.getProperty('innerText')
-// const srctxt = await src.jsonValue()
-// console.log({srctxt})
-//await page.waitFor(4000);
-//await page.click('a[title="Ingles"]')
+
 await page.evaluate(() => {
     doGTranslate('pt|en')
   });
@@ -22,7 +17,7 @@ const text6 = await page.evaluate(() => document.querySelectorAll("span.lbl-lici
 
 
 console.log( text1);
-console.log(text2);
+console.log(`Object: ${text2}`);
 console.log(text6);
 
 const [elements ]= await page.$x('//*[@id="block-views-block-view-noticia-pbh-block-5"]/div/div/div/div/div/div[3]/div/table/tbody/tr/td[2]/div/div/div/a')
@@ -34,9 +29,6 @@ console.log(`Related Links: ${imp_link}`)
 
 
 
-
-
- 
 
 
 scrapProduct("https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020")
